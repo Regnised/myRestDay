ac257333c05bf32f793fe12cdccd1080a8a2527b
Regnised <regnise@gmail.com>
2015-08-18 09:48:51 +0300

    Update restDay.php
    delete explode and mktime. -> add strtotime (cool function!)

--- restDay.php
+++ restDay.php
@@ -15,14 +15,15 @@
         <input type="text" name="myDate">
         <input type="submit">
     </form>
-    <?php
+<?php
 $regEx = preg_match("/\d{4}-\d{1,2}-\d{1,2}/", $_GET['myDate']);
-if (isset($_GET['myDate']) && $regEx !== false) {
-    $myDate  = $_GET['myDate'];
-    $cutDate = explode("-", $myDate);
-    $date    = date("w", mktime(0, 0, 0, $cutDate[1], $cutDate[2], $cutDate[0]));
+
+if (isset($_GET['myDate']) && $regEx != false) {
+    $date    = date("w", strtotime($_GET['myDate']));
     if ($date == 5 or $date == 6) {
-        echo "Выходной!";
+        echo "Rest day! =)";
+    } else {
+    	echo "Hard work! :(";
     }
 } else {
     echo 'Please, enter date for example: 2000-07-9';
